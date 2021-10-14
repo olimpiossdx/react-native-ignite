caa405e6b14beba65d559d3225db8e10563b8615
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-10-14 13:40:06 -0300

    Utilizando StyleSheet

--- myskills/src/pages/Home.js
+++ myskills/src/pages/Home.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native';
 
 export function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>React naive</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Jujé</Text>
       <Text>Ignite</Text>
+      <TextInput
+        placeholder="New skill"
+        placeholderTextColor="#555"
+        style={styles.input}
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>
+        My skills
+      </Text>
+
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    backgroundColor: '#121015',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',,
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+});

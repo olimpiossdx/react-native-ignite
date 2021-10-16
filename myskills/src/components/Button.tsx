7e34b8648a593716c280a5403651824efb3976f7
Olimpio Pimenta <olimpiodx@gmail.com>
2021-10-15 23:48:52 -0300

    Typando componentes com typescritp

---
+++ myskills/src/components/Button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+interface IButtonProps extends TouchableOpacityProps {
+  title: string;
+}
+
+export function Button({title, ...rest}: IButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button} activeOpacity={0.7} {...rest}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});

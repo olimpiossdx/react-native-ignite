58594154abf6fcabf82592f50a981d1c78f27dd0
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-10-25 13:54:08 -0300

    update and typing styleded flatlist

---
+++ chapter ii/gofinances/src/screens/Register/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export function Register() {
+  return (
+    <Container>
+      <Text>Register</Text>
+    </Container>
+  )
+};

ef8b9ee7d083f1cfe21d6ca9100324e3bb5e2088
Olimpio Pimenta <olimpiodx@gmail.com>
2021-10-25 19:36:23 -0300

    remove white space in px

--- chapter ii/gofinances/src/components/Form/TransactionTypeButton/sytles.ts
+++ chapter ii/gofinances/src/components/Form/TransactionTypeButton/sytles.ts
@@ -1,18 +1,18 @@
 import styled, { css } from "styled-components/native";
 import { TouchableOpacity } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
-import { Feather } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
 
 interface IconsProps {
-  type: 'up' | 'down';
+  type: "up" | "down";
 }
 
 interface ContainerProps {
   isActive: boolean;
-  type: 'up' | 'down';
+  type: "up" | "down";
 }
 
-export const Container = styled(TouchableOpacity) <ContainerProps>`
+export const Container = styled(TouchableOpacity)<ContainerProps>`
   width: 48%;
   background-color: ${({ theme }) => theme.colors.secondary.main};
 
@@ -20,26 +20,37 @@ export const Container = styled(TouchableOpacity) <ContainerProps>`
   align-items: center;
   justify-content: center;
 
-  border-width: ${({ isActive }) => isActive ? 0 : 1.5}px; 
+  border-width: ${({ isActive }) => (isActive ? 0 : 1.5)}px;
   border-style: solid;
   border-color: ${({ theme }) => theme.colors.text.main};
   border-radius: 5px;
 
   padding: 16px;
-  ${({ isActive, type }) => isActive && type === 'up' && css` background-color:${({ theme }) => theme.colors.success.light}`}
-  ${({ isActive, type }) => isActive && type === 'down' && css` background-color:${({ theme }) => theme.colors.attention.light}`}
+  ${({ isActive, type }) =>
+    isActive &&
+    type === "up" &&
+    css`
+      background-color: ${({ theme }) => theme.colors.success.light};
+    `}
+  ${({ isActive, type }) =>
+    isActive &&
+    type === "down" &&
+    css`
+      background-color: ${({ theme }) => theme.colors.attention.light};
+    `}
 `;
 
-export const Icon = styled(Feather) <IconsProps>`
-font-size: ${RFValue(24)} px;
-margin-right: 12px;
+export const Icon = styled(Feather)<IconsProps>`
+  font-size: ${RFValue(24)}px;
+  margin-right: 12px;
 
-color: ${({ theme, type }) => type === 'up' ? theme.colors.success.main : theme.colors.attention.main};
+  color: ${({ theme, type }) =>
+    type === "up" ? theme.colors.success.main : theme.colors.attention.main};
 `;
 
 export const Title = styled.Text`
-font-family: ${({ theme }) => theme.fonts.regular};
-font-size: ${RFValue(14)} px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
 
-color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme }) => theme.colors.shape};
 `;

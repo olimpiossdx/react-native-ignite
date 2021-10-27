05539f09a7f527a98fd36fe394e66629c4b703c8
Olimpio Pimenta <olimpiodx@gmail.com>
2021-10-27 18:58:51 -0300

    Fix run

--- chapter ii/gofinances/src/hooks/auth.tsx
+++ chapter ii/gofinances/src/hooks/auth.tsx
@@ -1,38 +1,37 @@
-import React, { createContext, ReactNode } from 'react';
-import { useContext } from 'hoist-non-react-statics/node_modules/@types/react';
+import React, { createContext, ReactNode, useContext } from "react";
 
 interface AuthProviderProps {
   children: ReactNode;
-};
+}
 
 interface User {
   id: string;
   name: string;
   email: string;
   photo?: string;
-};
+}
 
 interface AuthContextData {
   user: User;
-};
+}
 
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const user = {
-    id: '122',
-    name: 'jujé',
-    email: 'juje@teste.com'
+    id: "122",
+    name: "jujé",
+    email: "juje@teste.com",
   };
 
-  return (<AuthContext.Provider value={{ user }}>
-    {children}
-  </AuthContext.Provider>);
-};
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+}
 
 function useAuth() {
   const context = useContext(AuthContext);
   return context;
-};
+}
 
 export { AuthProvider, useAuth };
